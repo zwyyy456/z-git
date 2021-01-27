5c02752269ad47d1149000e5abc135fd4c5c28f8
zwy <zwyyy456@hotmail.com>
2021-01-27 21:34:18 +0800

    a test

---
+++ CppPrimerPlus/chapter3/test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	const int nInchPerFeet = 12;
+	cout << "please enter your height(inch)____\b\b\b\b";
+	int iHeightInch;
+	cin >> iHeightInch;
+	int iHeightFeet = iHeightInch / nInchPerFeet;
+	int remainder = iHeightInch % nInchPerFeet;
+	cout << "Height is " << iHeightFeet << " feet " << remainder << " inch\n";
+	return 0;
+	//test for git
+}
